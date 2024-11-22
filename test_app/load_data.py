681cb8d30848f393ad5a78a4c2914fb47936400a
IVadymMarchenko <142535938+IVadymMarchenko@users.noreply.github.com>
2024-11-23 01:34:21 +0200

    change load_data

--- test_app/load_data.py
+++ test_app/load_data.py
@@ -1,17 +1,21 @@
 import json
-import os
+from model import  BenchmarkingList
+import aiofiles
+from pydantic import ValidationError
 
-from model import Benchmark, BenchmarkingList
+async def load_data_json(filename):
+    async with aiofiles.open(filename, mode="r") as file:
+        content = await file.read()
+        return json.loads(content)
 
 
-def load_data_json(filename):
-    with open(filename, "r") as file:
-        data = json.load(file)
-        return data
+async def parsing_data(filename):
+    data =  await load_data_json(filename)
+    try:
+        validated_data = BenchmarkingList.model_validate(data)
+        return validated_data.benchmarking_results
+    except ValidationError as e:
+        raise ValueError(f"Data validation error:: {e}")
 
 
-def parsing_data(filename):
-    data = load_data_json(filename)
-    validated_data = BenchmarkingList.model_validate(data)
-    return validated_data.benchmarking_results
 
--- test_app/main.py
+++ test_app/main.py
@@ -15,7 +15,7 @@ DEBUG = os.getenv("SUPERBENCHMARK_DEBUG", "False") == "True"
 async def get_results_average():
     if not DEBUG:
         raise HTTPException(status_code=500, detail="the feature is not ready for live yet")
-    data = parsing_data("test_database.json")
+    data = await parsing_data("test_database.json")
     average_value_token_count = sum(result.token_count for result in data)
     average_value_time_to_first_token = sum(result.time_to_first_token for result in data) / len(data)
     average_value_time_per_output_token = sum(result.time_per_output_token for result in data) / len(data)
@@ -37,7 +37,7 @@ async def get_results_average():
 async def average_over_period_time(start_time: datetime, end_time: datetime):
     if not DEBUG:
         raise HTTPException(status_code=500, detail="the feature is not ready for live yet")
-    data = parsing_data("test_database.json")
+    data = await parsing_data("test_database.json")
     filtered_data = [result for result in data if start_time <= result.timestamp <= end_time]
     average_value_token_count = sum(result.token_count for result in filtered_data)
     average_value_time_to_first_token = sum(result.time_to_first_token for result in filtered_data) / len(filtered_data)
