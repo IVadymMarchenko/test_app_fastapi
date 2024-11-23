c4e2488b6ad86f51ea6ef5e1d2331e7f8d53b1ec
IVadymMarchenko <142535938+IVadymMarchenko@users.noreply.github.com>
2024-11-23 23:16:53 +0200

    change logic load data

--- test_app/load_data.py
+++ test_app/load_data.py
@@ -1,9 +1,10 @@
 import json
 import os
 
-from model import  Benchmark
+from model import Benchmark
 import aiofiles
 from pydantic import ValidationError
+
 
 async def load_data_json(filename):
     async with aiofiles.open(filename, mode="r") as file:
@@ -12,10 +13,12 @@ async def load_data_json(filename):
 
 
 async def parsing_data(filename):
-    data =  await load_data_json(filename)
+    data = await load_data_json(filename)
     try:
-        validated_data = [Benchmark.model_validate(benchmark_data) for benchmark_data in data.get("benchmarking_results", [])]
+        validated_data = [
+            Benchmark.model_validate(benchmark_data)
+            for benchmark_data in data.get("benchmarking_results", [])
+        ]
         return validated_data
     except ValidationError as e:
         raise ValueError(f"Data validation error:: {e}")
-
--- test_app/main.py
+++ test_app/main.py
@@ -14,14 +14,26 @@ DEBUG = os.getenv("SUPERBENCHMARK_DEBUG", "False") == "True"
 @app.get("/results/average")
 async def get_results_average():
     if not DEBUG:
-        raise HTTPException(status_code=500, detail="the application is not ready for start yet")
+        raise HTTPException(
+            status_code=500, detail="the application is not ready for start yet"
+        )
     data = await parsing_data("test_database.json")
     average_value_token_count = sum(result.token_count for result in data)
-    average_value_time_to_first_token = sum(result.time_to_first_token for result in data) / len(data)
-    average_value_time_per_output_token = sum(result.time_per_output_token for result in data) / len(data)
-    average_value_total_generation_time = sum(result.total_generation_time for result in data) / len(data)
-    average_value_prompt_text = sum(len(result.prompt_text) for result in data) / len(data)
-    average_value_generated_text = sum(len(result.generated_text) for result in data) / len(data)
+    average_value_time_to_first_token = sum(
+        result.time_to_first_token for result in data
+    ) / len(data)
+    average_value_time_per_output_token = sum(
+        result.time_per_output_token for result in data
+    ) / len(data)
+    average_value_total_generation_time = sum(
+        result.total_generation_time for result in data
+    ) / len(data)
+    average_value_prompt_text = sum(len(result.prompt_text) for result in data) / len(
+        data
+    )
+    average_value_generated_text = sum(
+        len(result.generated_text) for result in data
+    ) / len(data)
 
     return {
         "average_value_token_count": average_value_token_count,
@@ -36,18 +48,26 @@ async def get_results_average():
 @app.get("/results/average/{start_time}/{end_time}")
 async def average_over_period_time(start_time: datetime, end_time: datetime):
     if not DEBUG:
-        raise HTTPException(status_code=500, detail="the application is not ready for start yet")
+        raise HTTPException(
+            status_code=500, detail="the application is not ready for start yet"
+        )
     data = await parsing_data("test_database.json")
-    filtered_data = [result for result in data if start_time <= result.timestamp <= end_time]
+    filtered_data = [
+        result for result in data if start_time <= result.timestamp <= end_time
+    ]
     average_value_token_count = sum(result.token_count for result in filtered_data)
-    average_value_time_to_first_token = sum(result.time_to_first_token for result in filtered_data) / len(filtered_data)
-    average_value_time_per_output_token = sum(result.time_per_output_token for result in filtered_data) / len(
-        filtered_data
-    )
-    average_value_total_generation_time = sum(result.total_generation_time for result in filtered_data) / len(
-        filtered_data
-    )
-    average_value_prompt_text = sum(len(result.prompt_text) for result in filtered_data) / len(filtered_data)
+    average_value_time_to_first_token = sum(
+        result.time_to_first_token for result in filtered_data
+    ) / len(filtered_data)
+    average_value_time_per_output_token = sum(
+        result.time_per_output_token for result in filtered_data
+    ) / len(filtered_data)
+    average_value_total_generation_time = sum(
+        result.total_generation_time for result in filtered_data
+    ) / len(filtered_data)
+    average_value_prompt_text = sum(
+        len(result.prompt_text) for result in filtered_data
+    ) / len(filtered_data)
     return {
         "average_value_token_count": average_value_token_count,
         "average_value_time_to_first_token": average_value_time_to_first_token,
